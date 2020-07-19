ba6a10eede3079dcf2ed3b54df424689561c8266
OukaMiyuki <asukaloid.blog@gmail.com>
2020-07-20 06:54:52 +0700

    MongoDB_Percobaan-1_-_Koneksi Database + Database Schema

---
+++ index.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+//database connection
+const mongoDB = 'mongodb://localhost/playground';
+mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => console.log('Connected to MongoDB!'))
+    .catch(err => console.error('Could not connect to MongoDB Server : ', err));
+
+//Create database schema
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean
+}); 
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse(){
+    try{
+        const course = new Course({
+            name: 'Angular Course',
+            author: 'Ouka',
+            tags: ['Angular', 'frontend'],
+            isPublished: true
+        });
+    
+        const result = await course.save();
+        console.log(result);
+    } catch(err){
+        console.log('There\'s an error : ', err.message);
+    }
+}
+
+createCourse();
